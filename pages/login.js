b160cb4b1cf950ed7d1853f8b27c13b7e1c00cd0
Nam Nguyen <nhnam94@gmail.com>
2018-08-30 03:23:42 +0700

    define login component

--- pages/login.js
+++ pages/login.js
@@ -10,62 +10,8 @@ class LoginPage extends Page {
     return 'input[type="password"]'
   }
 
-  get emailAddress () {
-    return browser.element('input[type="email"]')
+  static emailField () {
+    return 'input[type="email"]'
   }
-
-  get password () {
-    return browser.element(LoginPage.passwordField())
-  }
-
-  get submitButton () {
-    return browser.element('button[type="submit"]')
-  }
-
-  get loginForm () {
-    return browser.element('form')
-  }
-
-  get errorMessage () {
-    return browser.element(LoginPage.errorMessage())
-  }
-
-  open () {
-    super.open(pageUrl)
-    browser.pause(1000) // Fixes issue with Chrome freezing
-  }
-
-  expectedPageUrl () {
-    return super.expectedPageUrl(pageUrl)
-  }
-
-  constructor () {
-    super()
-    this.waitForForm = () => {
-      return browser.waitForVisible('input[type="email"]', 30000)
-    }
-  }
-
-  doLogin (login, password) {
-    this.waitForForm()
-    this.emailAddress.click()
-    this.emailAddress.setValue(login)
-    this.password.click()
-    this.password.setValue(password)
-    this.clickSignIn()
-  }
-
-  waitForRoutesPageToLoad () {
-    routesPage.waitForPageToLoad()
-  }
-
-  clickSignIn () {
-    do {
-      browser.pause(200)
-      this.submitButton.click()
-    }
-    while (browser.isVisible(LoginPage.errorMessage()))
-  }
-
 }
 module.exports = new LoginPage()
